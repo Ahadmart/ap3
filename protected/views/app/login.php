d3a89866f0cd2fd2cb9550d55bb9c1a2ca0ad973
AbuMuhammad <b.hermianto@gmail.com>
2015-08-27 21:25:57 +0700

    Rapi2

--- protected/views/app/login.php
+++ protected/views/app/login.php
@@ -5,7 +5,7 @@
 
 $this->pageTitle = Yii::app()->name.' - Login';
 ?>
-<div class="row collapse"style="margin-top: 45px">
+<div class="row collapse" style="margin-top: 45px">
    <div class="small-12 medium-6 medium-offset-3 large-4 large-offset-4 columns">
       <div class="block">
          <div class="top-bar block-header">
@@ -14,53 +14,49 @@ $this->pageTitle = Yii::app()->name.' - Login';
             </ul>
          </div>
          <div class="block-content">
-            <div class="row collapse">
-               <div class="small-12 columns">
-                  <div class="form">
-                     <?php
-                     $form = $this->beginWidget('CActiveForm', array(
-                         'id' => 'login-form',
-                         //'enableClientValidation' => true,
-                         'clientOptions' => array(
-                             'validateOnSubmit' => true,
-                             'inputContainer' => '.input'
-                         ),
-                     ));
-                     ?>
-                     <div class="row">
-                        <div class="small-12 columns">
-                           <?php //echo $form->labelEx($model, 'username');  ?>
-                           <?php echo $form->textField($model, 'username', array('placeholder' => 'Nama User', 'accesskey' => 'n', 'autofocus' => 'autofocus', 'autocomplete' => 'off')); ?>
-                           <?php echo $form->error($model, 'username', array('class' => 'error')); ?>
-                        </div>
-                     </div>
-
-                     <div class="row">
-                        <div class="small-12 columns">
-                           <?php //echo $form->labelEx($model, 'password');  ?>
-                           <?php echo $form->passwordField($model, 'password', array('placeholder' => 'Password', 'accesskey' => 'p')); ?>
-                           <?php echo $form->error($model, 'password', array('class' => 'error')); ?>
-                        </div>
-                     </div>
-                     <?php
-                     /*
-                       <div class="row rememberMe">
-                       <div class="small-12 columns">
-                       <?php echo $form->checkBox($model, 'rememberMe'); ?>
-                       <?php echo $form->label($model, 'rememberMe'); ?>
-                       <?php echo $form->error($model, 'rememberMe'); ?>
-                       </div>
-                       </div>
-                      */
-                     ?>
-                     <div class="row">
-                        <div class="small-12 columns">
-                           <?php echo CHtml::submitButton('Login', array('class' => 'tiny bigfont button small-12')); ?>
-                        </div>
-                     </div>
-                     <?php $this->endWidget(); ?>
+            <div class="form">
+               <?php
+               $form = $this->beginWidget('CActiveForm', array(
+                   'id' => 'login-form',
+                   //'enableClientValidation' => true,
+                   'clientOptions' => array(
+                       'validateOnSubmit' => true,
+                       'inputContainer' => '.input'
+                   ),
+               ));
+               ?>
+               <div class="row">
+                  <div class="small-12 columns">
+                     <?php //echo $form->labelEx($model, 'username');  ?>
+                     <?php echo $form->textField($model, 'username', array('placeholder' => 'Nama User', 'accesskey' => 'n', 'autofocus' => 'autofocus', 'autocomplete' => 'off')); ?>
+                     <?php echo $form->error($model, 'username', array('class' => 'error')); ?>
                   </div>
                </div>
+
+               <div class="row">
+                  <div class="small-12 columns">
+                     <?php //echo $form->labelEx($model, 'password');  ?>
+                     <?php echo $form->passwordField($model, 'password', array('placeholder' => 'Password', 'accesskey' => 'p')); ?>
+                     <?php echo $form->error($model, 'password', array('class' => 'error')); ?>
+                  </div>
+               </div>
+               <?php
+               /*
+                 <div class="row rememberMe">
+                 <div class="small-12 columns">
+                 <?php echo $form->checkBox($model, 'rememberMe'); ?>
+                 <?php echo $form->label($model, 'rememberMe'); ?>
+                 <?php echo $form->error($model, 'rememberMe'); ?>
+                 </div>
+                 </div>
+                */
+               ?>
+               <div class="row">
+                  <div class="small-12 columns">
+                     <?php echo CHtml::submitButton('Login', array('class' => 'tiny bigfont button small-12')); ?>
+                  </div>
+               </div>
+               <?php $this->endWidget(); ?>
             </div>
          </div>
       </div>
