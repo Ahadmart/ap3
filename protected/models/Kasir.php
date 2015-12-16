8173701d9490207d306673054fac03ae3c5e5e74
abumuhammad <b.hermianto@gmail.com>
2015-12-16 14:55:55 +0700

    Kasir - tutup: penambahan perhitungan retur jual

--- protected/models/Kasir.php
+++ protected/models/Kasir.php
@@ -168,13 +168,14 @@ class Kasir extends CActiveRecord
             from penerimaan_detail d
             join penerimaan p on d.penerimaan_id = p.id and p.status=:statusPenerimaan
             join hutang_piutang hp on d.hutang_piutang_id = hp.id and hp.asal=:asalHutangPiutang
-            join penjualan on hp.id = penjualan.hutang_piutang_id and penjualan.tanggal>=:waktu
+            join penjualan on hp.id = penjualan.hutang_piutang_id and penjualan.tanggal>=:waktu and penjualan.updated_by=:userId
         ");
 
         $command->bindValues(array(
             ':waktu' => $this->waktu_buka,
             ':asalHutangPiutang' => HutangPiutang::DARI_PENJUALAN,
-            ':statusPenerimaan' => Penerimaan::STATUS_BAYAR
+            ':statusPenerimaan' => Penerimaan::STATUS_BAYAR,
+            ':userId' => $this->user_id
         ));
 
         return $command->queryRow();
@@ -187,7 +188,7 @@ class Kasir extends CActiveRecord
             from penerimaan_detail d
             join penerimaan p on d.penerimaan_id = p.id and p.status=:statusPenerimaan
             join hutang_piutang hp on d.hutang_piutang_id = hp.id and hp.asal=:asalHutangPiutang
-            join penjualan on hp.id = penjualan.hutang_piutang_id and penjualan.tanggal>=:waktu
+            join penjualan on hp.id = penjualan.hutang_piutang_id and penjualan.tanggal>=:waktu and penjualan.updated_by=:userId
             join penjualan_detail jual_detail on penjualan.id = jual_detail.penjualan_id
             join harga_pokok_penjualan hpp on jual_detail.id=hpp.penjualan_detail_id
         ");
@@ -195,7 +196,28 @@ class Kasir extends CActiveRecord
         $command->bindValues(array(
             ':waktu' => $this->waktu_buka,
             ':asalHutangPiutang' => HutangPiutang::DARI_PENJUALAN,
-            ':statusPenerimaan' => Penerimaan::STATUS_BAYAR
+            ':statusPenerimaan' => Penerimaan::STATUS_BAYAR,
+            ':userId' => $this->user_id
+        ));
+
+        return $command->queryRow();
+    }
+
+    public function totalReturJual()
+    {
+        $command = Yii::app()->db->createCommand("
+            select sum(d.jumlah) jumlah
+            from pengeluaran_detail d
+            join pengeluaran p on d.pengeluaran_id = p.id and p.status=:statusPengeluaran
+            join hutang_piutang hp on d.hutang_piutang_id = hp.id and hp.asal=:asalHutangPiutang
+            join retur_penjualan on hp.id = retur_penjualan.hutang_piutang_id and retur_penjualan.tanggal>=:waktu and retur_penjualan.updated_by=:userId
+        ");
+
+        $command->bindValues(array(
+            ':waktu' => $this->waktu_buka,
+            ':asalHutangPiutang' => HutangPiutang::DARI_RETUR_JUAL,
+            ':statusPengeluaran' => Pengeluaran::STATUS_BAYAR,
+            ':userId' => $this->user_id
         ));
 
         return $command->queryRow();
