97ae513a1aafb8fa6d8897b5125f2a08e098bcf6
abumuhammad <b.hermianto@gmail.com>
2016-03-08 10:02:40 +0700

    Import pembelian/penjualan: ditambahkan input database ahadpos2

--- protected/views/penjualan/import.php
+++ protected/views/penjualan/import.php
@@ -3,18 +3,24 @@ $this->boxHeader['small'] = 'Import';
 $this->boxHeader['normal'] = 'Import Penjualan dari AhadPOS 2';
 ?>
 <form method="POST">
-   <div class="row">
-      <div class="large-6 columns">
-         <div class="row collapse postfix-round">
-            <div class="small-9 columns">
-               <input type="text" name="nomor" placeholder="Masukkan Nomor Penjualan AhadPOS2" autofocus="autofocus">
+    <div class="row">
+        <div class="large-6 columns">
+            <div class="row collapse postfix-round">
+                <div class="small-9 columns">
+                    <input type="text" name="nomor" placeholder="Masukkan Nomor Penjualan AhadPOS2" autofocus="autofocus">
+                </div>
+                <div class="small-3 columns">
+                    <input class="button postfix" type="submit" value="Go" />
+                </div>
             </div>
-            <div class="small-3 columns">
-               <input class="button postfix" type="submit" value="Go" />
-            </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="medium-6 columns">
+            <label for="db_source">Database ahadPOS 2</label>
+            <input id="db_source" type="text" name="database" value="gudang" placeholder="Database ahadpos2 (sumber)" />
+        </div>
+    </div>
 </form>
 <?php
 $this->menu = array(
