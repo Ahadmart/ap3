4b871fca5754ed8a946a95abaa4e8b89d59a3397
abumuhammad <b.hermianto@gmail.com>
2016-03-31 16:14:53 +0700

    Cetak label rak (draft) : update

--- protected/views/cetaklabelrak/_barang.php
+++ protected/views/cetaklabelrak/_barang.php
@@ -3,14 +3,28 @@
 $this->widget('BGridView', array(
     'id' => 'label-rak-cetak-grid',
     'dataProvider' => $model->search(),
-    'filter' => $model,
+    'filter' => null,
     'columns' => array(
         //'barang_id',
         array(
+            'header' => 'Barcode',
             'value' => '$data->barang->barcode'
         ),
         array(
+            'header' => 'Nama',
             'value' => '$data->barang->nama'
+        ),
+        array(
+            'header' => 'Kategori',
+            'value' => '$data->barang->kategori->nama'
+        ),
+        array(
+            'header' => 'Satuan',
+            'value' => '$data->barang->satuan->nama'
+        ),
+        array(
+            'header' => 'Harga Jual',
+            'value' => '$data->barang->hargajual'
         ),
         //'updated_at',
         //'updated_by',
