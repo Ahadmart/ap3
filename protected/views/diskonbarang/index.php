e40cebf819ba58d14cb47adc21fc7ebc1329743f
abumuhammad <b.hermianto@gmail.com>
2016-06-10 10:33:00 +0700

    Fitur Diskon Promo Member : Semua Barang (update)

--- protected/views/diskonbarang/index.php
+++ protected/views/diskonbarang/index.php
@@ -43,12 +43,12 @@ $this->boxHeader['normal'] = 'Diskon Barang';
                     'filter' => $model->listTipeSort(),
                     'value' => '$data->namaTipeSort'
                 ),
-//                array(
-//                    'header' => 'Harga Asli',
-//                    'value' => '$data->barang->hargaJual',
-//                    'htmlOptions' => array('class' => 'rata-kanan'),
-//                    'headerHtmlOptions' => array('style' => 'width:75px', 'class' => 'rata-kanan'),
-//                ),
+                array(
+                    'header' => 'Harga Asli',
+                    'value' => '!is_null($data->barang) ? $data->barang->hargaJual : ""',
+                    'htmlOptions' => array('class' => 'rata-kanan'),
+                    'headerHtmlOptions' => array('style' => 'width:75px', 'class' => 'rata-kanan'),
+                ),
                 array(
                     'name' => 'nominal',
                     'htmlOptions' => array('class' => 'rata-kanan'),
