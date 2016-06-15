cf19bfcfa0309452a683ab551fa112e3765cd99e
abumuhammad <b.hermianto@gmail.com>
2016-06-15 10:39:27 +0700

    POS - ubah: Cari barang, klik langsung tambah barang

--- protected/views/pos/ubah.php
+++ protected/views/pos/ubah.php
@@ -172,6 +172,7 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->theme->baseUrl . '/js/v
                     "Nothing selected, input was " + this.value);
             if (ui.item) {
                 $("#scan").val(ui.item.value);
+                $("#tombol-tambah-barang").click();
             }
         }
     }).autocomplete("instance")._renderItem = function (ul, item) {
