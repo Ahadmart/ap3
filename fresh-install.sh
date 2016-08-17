6745e6a97ed799028c7eaff085445860c4399890
abumuhammad <b.hermianto@gmail.com>
2016-08-17 22:23:30 +0700

    Fresh Install: Update Compatibility

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,4 +1,5 @@
-export db=ahadpos3
-mysql -uroot -e"DROP SCHEMA IF EXISTS $db; CREATE SCHEMA $db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
-protected/yiic migrate
+#!/bin/bash
 
+export db=ahadpos3
+mysql -e"DROP SCHEMA IF EXISTS $db; CREATE SCHEMA $db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
+protected/yiic migrate
