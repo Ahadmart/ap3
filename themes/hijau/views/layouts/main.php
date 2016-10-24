805c3a77156fd82295627cf46c1f4fb5ec784e1d
abumuhammad <b.hermianto@gmail.com>
2016-10-24 13:50:35 +0700

    Tambah Menu Laporan Kartu Stok

--- themes/hijau/views/layouts/main.php
+++ themes/hijau/views/layouts/main.php
@@ -175,6 +175,7 @@
                                             array('label' => '<i class="fa fa-database fa-fw"></i>' . ' Pengeluaran/Penerimaan', 'url' => array('/report/pengeluaranpenerimaan')),
                                             array('label' => '<i class="fa fa-bar-chart fa-fw"></i>' . ' Umur Barang (Aging of Inventory)', 'url' => array('/report/umurbarang')),
                                             array('label' => '<i class="fa fa-bar-chart fa-fw"></i>' . ' Potensi Lost Sales', 'url' => array('/report/pls')),
+                                            array('label' => '<i class="fa fa-database fa-fw"></i>' . ' Kartu Stok', 'url' => array('/report/kartustok')),
                                         //array('label' => '<i class="fa fa-file-pdf-o fa-fw"></i>' . ' Harian Rekap', 'url' => array('/report/harianrekap')),
                                         ),
                                         'itemOptions' => array('class' => 'has-dropdown'),
