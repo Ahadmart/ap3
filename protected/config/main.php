ce2cde256d15255ce86fdca302f573c931feac91
abumuhammad <b.hermianto@gmail.com>
2017-11-09 16:47:22 +0700

    Update urlManager: Tambah path /tools

--- protected/config/main.php
+++ protected/config/main.php
@@ -38,6 +38,7 @@ return array(
             'showScriptName' => false,
             'caseSensitive' => false,
             'rules' => array(
+                'tools/<controller:\w+>/<action:\w+>' => 'tools/<controller>/<action>',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
