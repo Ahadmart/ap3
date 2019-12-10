228e4357f35e3918bbeba0e6ce040e680229da44
abumuhammad <b.hermianto@gmail.com>
2019-12-10 09:46:44 +0700

    Kas Bank Model: Auto Format

--- protected/models/KasBank.php
+++ protected/models/KasBank.php
@@ -15,56 +15,61 @@
  * @property KodeAkun $kodeAkun
  * @property User $updatedBy
  */
-class KasBank extends CActiveRecord {
+class KasBank extends CActiveRecord
+{
 
     /**
      * @return string the associated database table name
      */
-    public function tableName() {
+    public function tableName()
+    {
         return 'kas_bank';
     }
 
     /**
      * @return array validation rules for model attributes.
      */
-    public function rules() {
+    public function rules()
+    {
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
-        return array(
-            array('nama', 'required'),
-            array('nama', 'length', 'max' => 45),
-            array('kode_akun_id, updated_by', 'length', 'max' => 10),
-            array('created_at, updated_at, updated_by', 'safe'),
+        return [
+            ['nama', 'required'],
+            ['nama', 'length', 'max' => 45],
+            ['kode_akun_id, updated_by', 'length', 'max' => 10],
+            ['created_at, updated_at, updated_by', 'safe'],
             // The following rule is used by search().
             // @todo Please remove those attributes that should not be searched.
-            array('id, nama, kode_akun_id, created_at, updated_at, updated_by', 'safe', 'on' => 'search'),
-        );
+            ['id, nama, kode_akun_id, created_at, updated_at, updated_by', 'safe', 'on' => 'search'],
+        ];
     }
 
     /**
      * @return array relational rules.
      */
-    public function relations() {
+    public function relations()
+    {
         // NOTE: you may need to adjust the relation name and the related
         // class name for the relations automatically generated below.
-        return array(
-            'kodeAkun' => array(self::BELONGS_TO, 'KodeAkun', 'kode_akun_id'),
-            'updatedBy' => array(self::BELONGS_TO, 'User', 'updated_by'),
-        );
+        return [
+            'kodeAkun'  => [self::BELONGS_TO, 'KodeAkun', 'kode_akun_id'],
+            'updatedBy' => [self::BELONGS_TO, 'User', 'updated_by'],
+        ];
     }
 
     /**
      * @return array customized attribute labels (name=>label)
      */
-    public function attributeLabels() {
-        return array(
-            'id' => 'ID',
-            'nama' => 'Nama',
+    public function attributeLabels()
+    {
+        return [
+            'id'           => 'ID',
+            'nama'         => 'Nama',
             'kode_akun_id' => 'Kode Akun',
-            'created_at' => 'Created At',
-            'updated_at' => 'Updated At',
-            'updated_by' => 'Updated By',
-        );
+            'created_at'   => 'Created At',
+            'updated_at'   => 'Updated At',
+            'updated_by'   => 'Updated By',
+        ];
     }
 
     /**
@@ -79,7 +84,8 @@ class KasBank extends CActiveRecord {
      * @return CActiveDataProvider the data provider that can return the models
      * based on the search/filter conditions.
      */
-    public function search() {
+    public function search()
+    {
         // @todo Please modify the following code to remove attributes that should not be searched.
 
         $criteria = new CDbCriteria;
@@ -91,9 +97,9 @@ class KasBank extends CActiveRecord {
         $criteria->compare('updated_at', $this->updated_at, true);
         $criteria->compare('updated_by', $this->updated_by, true);
 
-        return new CActiveDataProvider($this, array(
+        return new CActiveDataProvider($this, [
             'criteria' => $criteria,
-        ));
+        ]);
     }
 
     /**
@@ -102,11 +108,13 @@ class KasBank extends CActiveRecord {
      * @param string $className active record class name.
      * @return KasBank the static model class
      */
-    public static function model($className = __CLASS__) {
+    public static function model($className = __CLASS__)
+    {
         return parent::model($className);
     }
 
-    public function beforeSave() {
+    public function beforeSave()
+    {
 
         if ($this->isNewRecord) {
             $this->created_at = date('Y-m-d H:i:s');
@@ -116,7 +124,8 @@ class KasBank extends CActiveRecord {
         return parent::beforeSave();
     }
 
-    public function beforeValidate() {
+    public function beforeValidate()
+    {
         if (empty($this->kode_akun_id)) {
             $this->kode_akun_id = null;
         }
