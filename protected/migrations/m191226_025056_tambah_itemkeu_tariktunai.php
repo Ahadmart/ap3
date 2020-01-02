f40326d70fc74b40293afd29159e4735548f5205
abumuhammad <b.hermianto@gmail.com>
2020-01-02 09:24:38 +0700

    DB-Update: Tambahan akun untuk Tarik Tunai Penerimaan

--- protected/migrations/m191226_025056_tambah_itemkeu_tariktunai.php
+++ protected/migrations/m191226_025056_tambah_itemkeu_tariktunai.php
@@ -5,19 +5,30 @@ class m191226_025056_tambah_itemkeu_tariktunai extends CDbMigration
 
     public function safeUp()
     {
-        
+
         $now  = date('Y-m-d H:i:s');
         $data = [
+            [
                 'id'         => 12,
                 'nama'       => 'Tarik Tunai via POS',
                 'parent_id'  => 8,
                 'jenis'      => 0,
                 'updated_at' => $now,
                 'updated_by' => 1,
-                'created_at' => $now,            
+                'created_at' => $now,
+            ],
+            [
+                'id'         => 13,
+                'nama'       => 'Tarik Tunai (Penerimaan) via POS',
+                'parent_id'  => 9,
+                'jenis'      => 1,
+                'updated_at' => $now,
+                'updated_by' => 1,
+                'created_at' => $now,
+            ],
         ];
 
-        $this->insert('item_keuangan', $data);
+        $this->insertMultiple('item_keuangan', $data);
     }
 
     public function safeDown()
