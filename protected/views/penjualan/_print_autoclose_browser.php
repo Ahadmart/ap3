c86ffd776e558235a67d3a15eed1513769834bb7
abumuhammad <b.hermianto@gmail.com>
2020-09-22 16:15:36 +0700

    Fix POS print via browser

--- protected/views/penjualan/_print_autoclose_browser.php
+++ protected/views/penjualan/_print_autoclose_browser.php
@@ -1,44 +1,58 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <style>
-            h1 {
-                margin-top: 10px;
-                font-size: 3.5rem;
-                text-align: center;
-                color: #fff;
-                font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-                font-style: normal;
-                font-weight: 300;;
+<?php
+/**
+ * https://github.com/Ahadmart/ap3/issues/24
+ * oleh: https://github.com/myzahron
+ */
+?>
+
+<head>
+    <style type="text/css">
+        @media print {
+            #printable {
+                display: block;
             }
-            body {
-                background-color : #0D47A1;
+
+            #nonPrintable {
+                display: none;
             }
-        </style>
-        <script type="text/javascript">
-            function printStruk(text) {
-                var printWindow = window.open();
-                printWindow.document.open('text/plain')
-                printWindow.document.write(text);
-                printWindow.document.close();
-                printWindow.focus();
-                printWindow.print();
-                printWindow.close();
-            }
-            setTimeout(
-                    function () {
-                        self.close();
-                        window.history.go(-1);
-                    },
-                    3000);
-        </script>
-    </head>
-    <body onload="printStruk(document.getElementsByTagName ('pre')[0].firstChild.data)">        
-        <h1>printing..</h1> 
-        <div style="background-color: #fff; color: #000; text-align: center; padding: 5px 0;">
-            <div style="display: inline-block; text-align: left">
-                <pre><?php echo $text; ?></pre>
-            </div>
+        }
+
+        h1 {
+            font-size: 3.5rem;
+            color: #fff;
+            font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+            font-style: normal;
+            font-weight: 300;
+        }
+
+        body {
+            background-color: #0D47A1;
+        }
+    </style>
+    <script>
+        function printMe() {
+            window.print();
+        }
+        setTimeout(
+            function() {
+                self.close();
+                window.history.go(-1);
+            },
+            3000);
+    </script>
+</head>
+
+<body onload="printMe()">
+    <div id="nonPrintable">
+        <h1>printing..</h1>
+    </div>
+    <div style="background-color: #fff; color: #000;" id="printable">
+        <div style="display: inline-block; text-align: left">
+            <pre><b><?php echo $text; ?></b></pre>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
