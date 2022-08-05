68bd90e653ae474f5b5effc184b758b03e78a9b8
abumuhammad <b.hermianto@gmail.com>
2022-08-05 13:17:13 +0700

    Barang-ubah: Update kondisi untuk struktur

--- protected/views/barang/_form.php
+++ protected/views/barang/_form.php
@@ -69,14 +69,14 @@
             <?php echo $form->error($model, 'variant_coefficient', ['class' => 'error']); ?>
             <?php
             /*
-            <?php echo $form->labelEx($model, 'restock_point'); ?>
-            <?php echo $form->textField($model, 'restock_point', array('size' => 10, 'maxlength' => 10)); ?>
-            <?php echo $form->error($model, 'restock_point', array('class' => 'error')); ?>
+                <?php echo $form->labelEx($model, 'restock_point'); ?>
+                <?php echo $form->textField($model, 'restock_point', array('size' => 10, 'maxlength' => 10)); ?>
+                <?php echo $form->error($model, 'restock_point', array('class' => 'error')); ?>
 
-            <?php echo $form->labelEx($model, 'restock_level'); ?>
-            <?php echo $form->textField($model, 'restock_level', array('size' => 10, 'maxlength' => 10)); ?>
-            <?php echo $form->error($model, 'restock_level', array('class' => 'error')); ?>
-            */
+                <?php echo $form->labelEx($model, 'restock_level'); ?>
+                <?php echo $form->textField($model, 'restock_level', array('size' => 10, 'maxlength' => 10)); ?>
+                <?php echo $form->error($model, 'restock_level', array('class' => 'error')); ?>
+                */
             ?>
             <?php echo $form->labelEx($model, 'status'); ?>
             <?php echo $form->dropDownList($model, 'status', ['1' => 'Aktif', '0' => 'Non Aktif']); ?>
@@ -84,104 +84,114 @@
 
         </div>
     </div>
-    <div class="row">
-        <div class="small-12 columns">
-            <label>Struktur: <span id="nama-struktur"><?= $model->getNamaStruktur() ?></span></label>
-        </div>
-        <div class="medium-4 columns" id="grid1-container">
-            <?php
-            $this->renderPartial('_grid1', [
-                'lv1' => $lv1,
-            ]);
-            ?>
-        </div>
-        <div class="medium-4 columns" id="grid2-container">
-            <?php
-            $this->renderPartial('_grid2', [
-                'lv2' => $strukturDummy,
-            ]);
-            ?>
-        </div>
-        <div class="medium-4 columns" id="grid3-container"">
+    <?php
+    if ($model->isNewRecord) {
+    ?>
+        <div class="row">
+            <div class="small-12 columns">
+                <label>Struktur: <span id="nama-struktur"><?= $model->getNamaStruktur() ?></span></label>
+            </div>
+            <div class="medium-4 columns" id="grid1-container">
+                <?php
+                $this->renderPartial('_grid1', [
+                    'lv1' => $lv1,
+                ]); ?>
+            </div>
+            <div class="medium-4 columns" id="grid2-container">
+                <?php
+                $this->renderPartial('_grid2', [
+                    'lv2' => $strukturDummy,
+                ]); ?>
+            </div>
+            <div class="medium-4 columns" id="grid3-container"">
         <?php
         $this->renderPartial('_grid3', [
             'lv3' => $strukturDummy,
-        ]);
-        ?>
+        ]); ?>
     </div>
     <?= $form->hiddenField($model, 'struktur_id') ?>
         </div>
+        <?php
+    }
+        ?>
+
 
         <div class=" row">
-            <div class="small-12 columns">
-                <?php echo CHtml::submitButton($model->isNewRecord ? 'Tambah' : 'Simpan', ['class' => 'tiny bigfont success button right']); ?>
-                <?php echo CHtml::link('Kembali', Yii::app()->request->urlReferrer, ['class' => 'tiny bigfont button']); ?>
+                <div class="small-12 columns">
+                    <?php echo CHtml::submitButton($model->isNewRecord ? 'Tambah' : 'Simpan', ['class' => 'tiny bigfont success button right']); ?>
+                    <?php echo CHtml::link('Kembali', Yii::app()->request->urlReferrer, ['class' => 'tiny bigfont button']); ?>
+                </div>
             </div>
+
+            <?php $this->endWidget(); ?>
+
         </div>
-
-        <?php $this->endWidget(); ?>
-
-    </div>
-</div>
-<script>
-    $("#Barang_barcode").keydown(function(e) {
-        if (e.keyCode == 13) {
-            //$("#Barang_kategori_id").focus();
-            $(this).next().focus(); //Use whatever selector necessary to focus the 'next' input
-            return false;
-        }
-    });
-
-    function lv1Dipilih(id) {
-        var lv1Id = $('#' + id).yiiGridView('getSelection');
-        if (!Array.isArray(lv1Id) || !lv1Id.length) {
-            console.log("1 tidak dipilih");
-            <?php /* render nothing */ ?>
-            $("#grid2-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
-                level: 2,
-                parent: 0
+        <script>
+            $("#Barang_barcode").keydown(function(e) {
+                if (e.keyCode == 13) {
+                    //$("#Barang_kategori_id").focus();
+                    $(this).next().focus(); //Use whatever selector necessary to focus the 'next' input
+                    return false;
+                }
             });
-            $('#Barang_struktur_id').val("");
-        } else {
-            console.log(lv1Id[0] + ":1 dipilih");
-            $("#grid2-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
-                level: 2,
-                parent: lv1Id[0]
-            });
-        }
-        $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
-            level: 3,
-            parent: 0
-        });
-    }
 
-    function lv2Dipilih(id) {
-        var lv2Id = $('#' + id).yiiGridView('getSelection');
-        if (!Array.isArray(lv2Id) || !lv2Id.length) {
-            console.log("2 tidak dipilih");
-            <?php /* render nothing */ ?>
-            $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
-                level: 3,
-                parent: 0
-            });
-            $('#Barang_struktur_id').val("");
-        } else {
-            console.log(lv2Id[0] + ":2 dipilih");
-            $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
-                level: 3,
-                parent: lv2Id[0]
-            });
-        }
-    }
+            <?php
+            if ($model->isNewRecord) {
+            ?>
 
-    function lv3Dipilih(id) {
-        var lv3Id = $('#' + id).yiiGridView('getSelection');
-        if (!Array.isArray(lv3Id) || !lv3Id.length) {
-            console.log("3 tidak dipilih");
-            $('#Barang_struktur_id').val("");
-        } else {
-            console.log(lv3Id[0] + ":3 dipilih");
-            $('#Barang_struktur_id').val(lv3Id[0]);
-        }
-    }
-</script>
+                function lv1Dipilih(id) {
+                    var lv1Id = $('#' + id).yiiGridView('getSelection');
+                    if (!Array.isArray(lv1Id) || !lv1Id.length) {
+                        console.log("1 tidak dipilih");
+                        <?php /* render nothing */ ?>
+                        $("#grid2-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
+                            level: 2,
+                            parent: 0
+                        });
+                        $('#Barang_struktur_id').val("");
+                    } else {
+                        console.log(lv1Id[0] + ":1 dipilih");
+                        $("#grid2-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
+                            level: 2,
+                            parent: lv1Id[0]
+                        });
+                    }
+                    $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
+                        level: 3,
+                        parent: 0
+                    });
+                }
+
+                function lv2Dipilih(id) {
+                    var lv2Id = $('#' + id).yiiGridView('getSelection');
+                    if (!Array.isArray(lv2Id) || !lv2Id.length) {
+                        console.log("2 tidak dipilih");
+                        <?php /* render nothing */ ?>
+                        $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
+                            level: 3,
+                            parent: 0
+                        });
+                        $('#Barang_struktur_id').val("");
+                    } else {
+                        console.log(lv2Id[0] + ":2 dipilih");
+                        $("#grid3-container").load("<?= $this->createUrl('renderstrukturgrid') ?>", {
+                            level: 3,
+                            parent: lv2Id[0]
+                        });
+                    }
+                }
+
+                function lv3Dipilih(id) {
+                    var lv3Id = $('#' + id).yiiGridView('getSelection');
+                    if (!Array.isArray(lv3Id) || !lv3Id.length) {
+                        console.log("3 tidak dipilih");
+                        $('#Barang_struktur_id').val("");
+                    } else {
+                        console.log(lv3Id[0] + ":3 dipilih");
+                        $('#Barang_struktur_id').val(lv3Id[0]);
+                    }
+                }
+            <?php
+            }
+            ?>
+        </script>
