601f32eb4eded10b6b25e3fc8a40be4b96dd276f
abumuhammad <b.hermianto@gmail.com>
2023-08-09 12:53:59 +0700

    Modul pajak: Rapi-rapi form

--- protected/views/ppnpembelian/_form.php
+++ protected/views/ppnpembelian/_form.php
@@ -18,24 +18,22 @@
 	<?php echo $form->errorSummary($model, 'Error: Perbaiki input', null, ['class' => 'panel callout']); ?>
 
 	<?php echo $form->hiddenField($model, 'pembelian_id'); ?>
-	<div class="small-12 columns">
-		<div class="row collapse">
-			<label>Pembelian</label>
-			<div class="small-8 columns">
-				<?php echo CHtml::textField('nomorpembelian', empty($model->pembelian_id) ? '' : $model->pembelian->nomor, ['size' => 60, 'maxlength' => 500, 'disabled' => 'disabled']); ?>
-			</div>
-			<div class="small-1 columns">
-				<a class="tiny bigfont secondary button postfix" id="tombol-hapuspembelian"><i class="fa fa-eraser"></i></a>
-			</div>
-			<div class="small-3 columns">
-				<a class="tiny bigfont button postfix" id="tombol-browse" accesskey="p"><span class="ak">P</span>ilih..</a>
+	<div class="row">
+		<div class="medium-8 columns">
+			<div class="row collapse">
+				<label>Pembelian</label>
+				<div class="small-8 columns">
+					<?php echo CHtml::textField('nomorpembelian', empty($model->pembelian_id) ? '' : $model->pembelian->nomor, ['size' => 60, 'maxlength' => 500, 'disabled' => 'disabled']); ?>
+				</div>
+				<div class="small-1 columns">
+					<a class="tiny bigfont secondary button postfix" id="tombol-hapuspembelian"><i class="fa fa-eraser"></i></a>
+				</div>
+				<div class="small-3 columns">
+					<a class="tiny bigfont button postfix" id="tombol-browse" accesskey="p"><span class="ak">P</span>ilih..</a>
+				</div>
 			</div>
 		</div>
-	</div>
-
-
-	<div class="row">
-		<div class="small-12 columns">
+		<div class="medium-4 columns">
 			<?php echo $form->labelEx($model, 'total_ppn_hitung'); ?>
 			<?php echo $form->textField($model, 'total_ppn_hitung', ['size' => 18, 'maxlength' => 18, 'disabled' => 'disabled']); ?>
 			<?php echo $form->error($model, 'total_ppn_hitung', ['class' => 'error']); ?>
@@ -43,7 +41,7 @@
 	</div>
 
 	<div class="row">
-		<div class="small-12 columns">
+		<div class="medium-8 columns">
 			<?php echo $form->labelEx($model, 'no_faktur_pajak'); ?>
 			<?php echo $form->textField($model, 'no_faktur_pajak', [
 				'size'        => 45,
@@ -54,10 +52,7 @@
 			]); ?>
 			<?php echo $form->error($model, 'no_faktur_pajak', ['class' => 'error']); ?>
 		</div>
-	</div>
-
-	<div class="row">
-		<div class="small-12 columns">
+		<div class="medium-4 columns">
 			<?php echo $form->labelEx($model, 'total_ppn_faktur'); ?>
 			<?php echo $form->textField($model, 'total_ppn_faktur', ['size' => 18, 'maxlength' => 18]); ?>
 			<?php echo $form->error($model, 'total_ppn_faktur', ['class' => 'error']); ?>
