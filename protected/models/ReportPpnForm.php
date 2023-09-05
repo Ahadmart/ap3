f9777e326ef966ed3d7dae2d4f31cc00b7d22ef3
abumuhammad <b.hermianto@gmail.com>
2023-09-05 10:21:43 +0700

    Modul pajak: Report Ppn (update)

--- protected/models/ReportPpnForm.php
+++ protected/models/ReportPpnForm.php
@@ -37,7 +37,10 @@ class ReportPpnForm extends CFormModel
     public function reportPpn()
     {
         return [
-            'totalPpnPembelianPending' => $this->totalPpnPembelianPending()
+            'totalPpnPembelianPending'  => $this->totalPpnPembelianPending(),
+            'totalPpnPembelianValid'    => $this->totalPpnPembelianValid(),
+            'detailPpnPembelianPending' => $this->detailPpnPembelianPending(),
+            'detailPpnPembelianValid'   => $this->detailPpnPembelianValid(),
         ];
     }
 
@@ -45,7 +48,7 @@ class ReportPpnForm extends CFormModel
     {
         // Fix me: Masih kurang kondisi periode
         $sql = '
-        SELECT 
+        SELECT
             SUM(total_ppn_hitung) total
         FROM
             pembelian_ppn
@@ -56,10 +59,86 @@ class ReportPpnForm extends CFormModel
         $command = Yii::app()->db->createCommand($sql);
 
         $command->bindValues([
-            ':statusPending' => PembelianPpn::STATUS_PENDING
+            ':statusPending' => PembelianPpn::STATUS_PENDING,
         ]);
 
         $r = $command->queryRow();
         return $r['total'];
+    }
+
+    public function totalPpnPembelianValid()
+    {
+        // Fix me: Masih kurang kondisi periode
+        $sql = '
+        SELECT
+            SUM(total_ppn_faktur) total
+        FROM
+            pembelian_ppn
+        WHERE
+            status = :statusValid
+        ';
+
+        $command = Yii::app()->db->createCommand($sql);
+
+        $command->bindValues([
+            ':statusValid' => PembelianPpn::STATUS_VALID,
+        ]);
+
+        $r = $command->queryRow();
+        return $r['total'];
+    }
+
+    public function detailPpnPembelianPending()
+    {
+        $sql = '
+        SELECT
+            profil.nama,
+            no_faktur_pajak,
+            pembelian.nomor,
+            total_ppn_hitung AS jumlah
+        FROM
+            pembelian_ppn t
+                JOIN
+            pembelian ON pembelian.id = t.pembelian_id
+                JOIN
+            profil ON profil.id = pembelian.profil_id
+        WHERE
+            t.status = :statusPending
+        ';
+
+        $command = Yii::app()->db->createCommand($sql);
+
+        $command->bindValues([
+            ':statusPending' => PembelianPpn::STATUS_PENDING,
+        ]);
+
+        return $command->queryAll();
+    }
+
+    public function detailPpnPembelianValid()
+    {
+        $sql = '
+        SELECT
+            profil.nama,
+            no_faktur_pajak,
+            pembelian.nomor,
+            total_ppn_faktur AS jumlah
+        FROM
+            pembelian_ppn t
+                JOIN
+            pembelian ON pembelian.id = t.pembelian_id
+                JOIN
+            profil ON profil.id = pembelian.profil_id
+        WHERE
+            t.status = :statusValid
+        ';
+
+        $command = Yii::app()->db->createCommand($sql);
+
+        $command->bindValues([
+            ':statusValid' => PembelianPpn::STATUS_VALID,
+        ]);
+
+        return $command->queryAll();
     }
 }
