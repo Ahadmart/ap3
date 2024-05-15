9c236e41d527f3001d0bc196575ec3cd73a7f1b8
abumuhammad <b.hermianto@gmail.com>
2024-05-15 17:26:55 +0700

    Brosur Promo: Upload + delete file

--- protected/controllers/BrosurpromoController.php
+++ protected/controllers/BrosurpromoController.php
@@ -3,53 +3,139 @@
 class BrosurpromoController extends Controller
 {
 	// public $layout = '//layouts/box_kecil';
-	public function actionIndex()
+	public $assetsPath   = 'assets/brosurpromo/';
+	public $assetsPathTh = 'assets/brosurpromo/th/';
+
+	protected function ambilBrosur()
 	{
-		$assetsPath = 'assets/brosurpromo/';
-		$assetsPathTh = 'assets/brosurpromo/th/';
 		$imgs = [];
-		foreach (glob($assetsPath . '*.{jpg,JPG,jpeg,JPEG,png,PNG}', GLOB_BRACE) as $filename) {
+		foreach (glob($this->assetsPath . '*.*', GLOB_BRACE) as $filename) {
 			$imgs[] = [
 				'filename' => basename($filename),
 				'realpath' => realpath($filename),
 			];
 		}
+		return $imgs;
+	}
 
-		// echo '<pre>';
-		// print_r($imgs);
-		// echo '</pre>';
-
+	public function actionIndex()
+	{
 		$this->render('index', [
-			'assetsPath' => $assetsPath,
-			'assetsPathTh' => $assetsPathTh,
-			'imgs' => $imgs
+			'assetsPath'   => $this->assetsPath,
+			'assetsPathTh' => $this->assetsPathTh,
+			'imgs'         => $this->ambilBrosur(),
 		]);
+	}
+
+	public function actionUploadBrosur()
+	{
+		require_once __DIR__ . '/../vendor/autoload.php';
+		// echo ('Dari upload Brosur');
+		// echo '<pre>';
+		// print_r($_FILES);
+		foreach ($_FILES as $brosur) {
+			// print_r($brosur);
+			$file = new CUploadedFile(
+				$brosur['name'],
+				$brosur['tmp_name'],
+				$brosur['type'],
+				$brosur['size'],
+				$brosur['error']
+			);
+			$now = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+			$now->setTimezone(new DateTimeZone('Asia/Jakarta'));
+			$timestamp    = $now->format('m-d-YTH:i:s.uP');
+			$fileName     = 'brosur-' . $timestamp;
+			$fullFilePath = realpath($this->assetsPath) . '/' . $fileName . '.' . $file->extensionName;
+			// echo $fullFilePath;
+			// echo Yii::app()->basePath;
+			if (!is_dir(realpath($this->assetsPath))) {
+				mkdir($this->assetsPath);
+			}
+			if ($file->saveAs($fullFilePath)) {
+				$imagine   = new \Imagine\Gd\Imagine();
+				$image     = $imagine->open($fullFilePath);
+				$thumbnail = $image->thumbnail(new \Imagine\Image\Box(200, 200));
+				if (!is_dir(realpath($this->assetsPathTh))) {
+					mkdir($this->assetsPathTh);
+				}
+				$thumbnail->save(realpath($this->assetsPathTh) . '/' . $fileName . '.' . $file->extensionName);
+			} else {
+				echo 'Gagal simpan ke: ' . $fullFilePath;
+			}
+		}
+		// echo '</pre>';
+	}
+
+	public function actionLoadBrosur()
+	{
+		$this->renderPartial('_brosur', [
+			'assetsPath'   => $this->assetsPath,
+			'assetsPathTh' => $this->assetsPathTh,
+			'imgs'         => $this->ambilBrosur(),
+		]);
+	}
+
+	public function actionHapus()
+	{
+		if (isset($_POST['filename'])) {
+			$fileName = $_POST['filename'];
+			$r1       = $this->hapusFile(realpath($this->assetsPath) . '/' . $fileName);
+			$r2       = $this->hapusFile(realpath($this->assetsPathTh) . '/' . $fileName);
+		}
+
+		if ($r1 === 0 and $r2 === 0) {
+			$h = [
+				'sukses' => true,
+			];
+		} else {
+			$h = [
+				'sukses' => false,
+			];
+		}
+		$this->renderJSON($h);
+	}
+
+	protected function hapusFile($file)
+	{
+		if (file_exists($file)) {
+			if (unlink($file)) {
+				// echo $file . ' unlinked successfully';
+				return 0;
+			} else {
+				// echo 'Unable to unlink ' . $file;
+				return 1;
+			}
+		} else {
+			// echo 'Unable to unlink: ' . $file . '. File does not exist!';
+			return 2;
+		}
 	}
 
 	// Uncomment the following methods and override them if needed
 	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
+public function filters()
+{
+// return the filter configuration for this controller, e.g.:
+return array(
+'inlineFilterName',
+array(
+'class'=>'path.to.FilterClass',
+'propertyName'=>'propertyValue',
+),
+);
+}
 
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+public function actions()
+{
+// return external action classes, e.g.:
+return array(
+'action1'=>'path.to.ActionClass',
+'action2'=>array(
+'class'=>'path.to.AnotherActionClass',
+'propertyName'=>'propertyValue',
+),
+);
+}
+ */
 }
--- protected/views/brosurpromo/index.php
+++ protected/views/brosurpromo/index.php
@@ -1,21 +1,26 @@
 <?php
 /* @var $this BrosurpromoController */
 
-$this->breadcrumbs = array(
+$this->breadcrumbs = [
 	'Brosurpromo',
-);
-$this->boxHeader['small'] = 'Brosur Promo';
+];
+$this->boxHeader['small']  = 'Brosur Promo';
 $this->boxHeader['normal'] = 'Brosur Promo';
 ?>
 <style>
 	.brosur-card-container {
 		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
 		gap: 20px;
+		padding: 20px 0;
 	}
 
 	.brosur-card {
-		width: 200px;
-		/* height: 400px; */
+		width: 203px;
+		height: 203px;
+		border: thin solid #002c5e;
+		border-radius: 5px;
 	}
 
 	.brosur-card .img {
@@ -23,17 +28,16 @@ $this->boxHeader['normal'] = 'Brosur Promo';
 		width: 200px;
 		display: flex;
 		align-items: center;
+		/* border: thin solid white; */
 	}
 
-	.tombol-brosur {
-		display: flex;
-		flex-direction: row;
-		gap: 2%;
+	.img>a {
+		margin: auto;
 	}
 
-	.tombol-brosur>a,
-	.tombol-brosur>label {
-		width: 49%;
+	.tombol-hapus {
+		top: -97%;
+		left: 72%;
 	}
 
 	input[type="file"] {
@@ -44,27 +48,29 @@ $this->boxHeader['normal'] = 'Brosur Promo';
 		font-size: 0.875rem;
 	}
 </style>
-
+<!-- <div class="row">
+	<div class="small-12 column">
+		<div class="panel">
+			<p>Drop image here</p>
+		</div>
+	</div>
+</div> -->
+<div class="row">
+	<div class="small-12 column">
+		<form id="upload-brosur" action="<?= $this->createUrl('uploadbrosur') ?>" method='POST' enctype="multipart/form-data">
+			<input type="file" name="gambar-brosur" id="file" multiple="true" accepts="image/*" />
+		</form>
+	</div>
+</div>
 <div class="row">
 	<div class="small-12 column">
 		<div class="brosur-card-container">
 			<?php
-			foreach ($imgs as $img) {
-			?>
-				<div class="brosur-card">
-					<div class="img">
-						<a href="<?= $assetsPath . $img['filename'] ?>"><img src="<?= $assetsPathTh . $img['filename'] ?>"></a>
-					</div>
-					<div class="tombol-brosur">
-						<a class="tiny bigfont button">Hapus</a>
-						<label for="file_<?= $img['filename'] ?>" class="custom-upload" onclick="showFilesGanti('file_<?= $img['filename'] ?>')">Ganti</label>
-					</div>
-					<form action="<?= $this->createUrl('upload', ['asal' => $img['filename']]) ?>" method='POST' enctype="multipart/form-data">
-						<input name="file_<?= $img['filename'] ?>" type="file" onchange="this.form.submit()" />
-					</form>
-				</div>
-			<?php
-			}
+			$this->renderPartial('_brosur', [
+				'assetsPath'   => $assetsPath,
+				'assetsPathTh' => $assetsPathTh,
+				'imgs'         => $imgs,
+			]);
 			?>
 		</div>
 	</div>
@@ -72,5 +78,78 @@ $this->boxHeader['normal'] = 'Brosur Promo';
 <script>
 	function showFilesGanti(name) {
 		$("input[name='" + name + "']").click();
+		return false;
+	}
+	$(document).on("change", "input[name^='gambar-brosur']", function(e) {
+		e.preventDefault();
+		$("#upload-brosur").trigger('submit')
+	})
+	$("#upload-brosur").submit(function(e) {
+		e.preventDefault();
+		var data = new FormData();
+		jQuery.each(jQuery('#file')[0].files, function(i, file) {
+			data.append('brosur' + i, file);
+		});
+		uploadFile('<?= $this->createUrl('uploadbrosur') ?>', data);
+	})
+
+	function uploadFile(uploadurl, fileuploaddata) {
+		console.log('AJAX Upload');
+		$.ajax({
+			// xhr: function() {
+			// 	var xhr = new window.XMLHttpRequest();
+
+			// 	xhr.upload.addEventListener("progress", function(evt) {
+			// 		if (evt.lengthComputable) {
+			// 			var percentComplete = evt.loaded / evt.total;
+			// 			percentComplete = parseInt(percentComplete * 100);
+			// 			console.log(percentComplete);
+
+			// 			if (percentComplete === 100) {
+
+			// 			}
+
+			// 		}
+			// 	}, false);
+
+			// 	return xhr;
+			// },
+			url: uploadurl,
+			type: "POST",
+			data: fileuploaddata,
+			processData: false,
+			contentType: false,
+			success: function(result) {
+				$(".brosur-card-container").load('<?= $this->createUrl('loadbrosur') ?>');
+			}
+		});
 	}
 </script>
+
+<?php
+$this->menu = [
+	['itemOptions' => ['class' => 'divider'], 'label' => ''],
+	[
+		'itemOptions'          => ['class' => 'has-form hide-for-small-only'], 'label' => '',
+		'items'             => [
+
+			['label' => '<i class="fa fa-plus"></i> <span class="ak">U</span>pload', 'url' => '', 'linkOptions' => [
+				'class'     => 'button',
+				'accesskey' => 'u',
+				'onclick' => "return showFilesGanti('gambar-brosur')"
+			]],
+		],
+		'submenuOptions'    => ['class' => 'button-group'],
+	],
+	[
+		'itemOptions'          => ['class' => 'has-form show-for-small-only'], 'label' => '',
+		'items'             => [
+			['label' => '<i class="fa fa-plus"></i>', 'url' => '', 'linkOptions' => [
+				'class' => 'button',
+				'onclick' => "return showFilesGanti('gambar-brosur')"
+			]],
+		],
+		'submenuOptions'    => ['class' => 'button-group'],
+	],
+];
+?>
