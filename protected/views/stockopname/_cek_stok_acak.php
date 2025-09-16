90cf5a7fc3f54b07f6f7ed7243f430f790e7ce6c
abumuhammad <b.hermianto@gmail.com>
2025-09-16 20:28:39 +0700

    SO: fitur Cek Stok Acak (Draft)

--- protected/views/stockopname/_cek_stok_acak.php
+++ protected/views/stockopname/_cek_stok_acak.php
@@ -1,5 +1,5 @@
 <div class="row">
     <div class="small 12 column">
-        <a class="tiny bigfont button">Cek Stok Acak..</a>
+        <a class="tiny bigfont button">Sampel Acak..</a>
     </div>
 </div>
